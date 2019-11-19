44ed23cdd0be2e8ecc425607da996bb32eed9748
aleev_b <b.aleev@krlb.ru>
2019-11-19 10:55:42 +0300

    added testUtilities and browser factory

--- src/main/java/com.herokuapp.theInternet.base/BaseTest.java
+++ src/main/java/com.herokuapp.theInternet.base/BaseTest.java
@@ -1,8 +1,6 @@
 package com.herokuapp.theInternet.base;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
